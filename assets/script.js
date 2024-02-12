d42a43451679b4746995a01a10106cd6047245bf
Alyssa Hanewinkel <Tulio@alyssas-mbp.lan>
2024-02-12 02:26:46 -0600

    <created topic lists and added function to js script>

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,24 @@
-/*
-const topic = "HTML";
-
-if (topic === 'HTML') {
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
+function selectTopic() {
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomtopic === 'JavaScript') {
     console.log("Let's study JavaScript!")
 } else {
     console.log('Please try again!')
+    }
 }
-*/
-
-const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+console.log('Which topic should we study first?');
+selectTopic()
